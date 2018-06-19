c7972c60edf2af11cd9e38eec139b9d8f00f7b3f
eckmLJE <lucas.eckman@gmail.com>
2018-06-19 17:37:33 -0400

    last changes for day

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 apartment = Apartment.create(name: "1", floor: 1);
-bedroom = Bedroom.create(apartment: apartment, price: 1750, lease_start: "2018-07-01", term: 12, name: "1a")
+bedroom = Bedroom.create(apartment: apartment, price: 1750, lease_start: "2017-06-01", term: 12, name: "1a")
 bedroom2 = Bedroom.create(apartment: apartment, price: 2000, lease_start: "2018-06-01", term: 6, name: "1b")
 bedroom3 = Bedroom.create(apartment: apartment, price: 2250, lease_start: "2018-01-01", term: 6, name: "1c")
 bedroom4 = Bedroom.create(apartment: apartment, price: 2500, lease_start: "2018-09-01", term: 12, name: "1d")
