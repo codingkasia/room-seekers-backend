cdc1f68968706d915415cb0dde939371f65ff91e
codingkasia <codingkasia@gmail.com>
2018-08-19 23:59:37 -0400

    deployemnt

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module RoomSeek
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     
-    config.middleware.insert_before 0, Rack::Cors do
+ config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', headers: :any, methods: [:get, :post, :options]
@@ -38,6 +38,8 @@ module RoomSeek
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+
+    #?
+    # config.api_only = true
   end
 end
